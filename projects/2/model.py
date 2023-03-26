ac2fc5799fb434b2f3b33ae11a5a65642be1008d
zakharovart01 <100358898+zakharovart01@users.noreply.github.com>
2023-03-26 19:09:49 +0300

    Update model.py

--- projects/2/model.py
+++ projects/2/model.py
@@ -4,24 +4,27 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.tree import DecisionTreeClassifier
 
 #
 # Dataset fields
 #
 
+numeric_features = ["if" + str(i) for i in range(1, 14)]
+categorical_features = ["day_number"]
+fields = ["id", "label"] + numeric_features + categorical_features
+
+real_numeric_features = ["if" + str(i) for i in range(1, 14)]
+real_categorical_features = ["cf" + str(i) for i in range(1, 27)] + ["day_number"]
+real_fields = ["id", "label"] + real_numeric_features + real_categorical_features
 
 #
 # Model pipeline
 #
 
 # We create the preprocessing pipelines for both numeric and categorical data.
-numeric_features = ["if"+str(i) for i in range(1,14)]
-categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
-
 numeric_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='median')),
-#    ('scaler', StandardScaler())
+   ('scaler', StandardScaler())
 ])
 
 categorical_transformer = Pipeline(steps=[
@@ -29,18 +32,15 @@ categorical_transformer = Pipeline(steps=[
     ('onehot', OneHotEncoder(handle_unknown='ignore'))
 ])
 
-fields = ["id", "label"] + numeric_features + categorical_features
-fields_val = ["id"] + numeric_features + categorical_features
-
 preprocessor = ColumnTransformer(
     transformers=[
         ('num', numeric_transformer, numeric_features),
-        ('cat', 'drop', categorical_features)
+        ('cat', categorical_transformer, categorical_features)
     ]
 )
 
 # Now we have a full prediction pipeline.
 model = Pipeline(steps=[
     ('preprocessor', preprocessor),
-    ('logregression', LogisticRegression())
+    ('LogisticRegression', LogisticRegression(verbose=1, max_iter=1000))
 ])
