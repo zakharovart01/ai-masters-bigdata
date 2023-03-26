2b21e76bc7feb2b7cf2838938e1f29c00feaa040
zakharovart01 <100358898+zakharovart01@users.noreply.github.com>
2023-03-26 19:10:54 +0300

    Update train.py

--- projects/2/train.py
+++ projects/2/train.py
@@ -5,14 +5,13 @@ import logging
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import log_loss
 from joblib import dump
-import numpy as np
 
 #
 # Import model definition
 #
-from model import model, fields
+from model import model, fields, real_fields
+
 
 #
 # Logging initialization
@@ -26,9 +25,8 @@ logging.info("ARGS {}".format(sys.argv[1:]))
 # Read script arguments
 #
 try:
-  proj_id = sys.argv[1] 
-#   train_path = sys.argv[2]
-  train_path='/home/users/datasets/criteo/criteo_train500'
+  proj_id = sys.argv[1]
+  train_path = sys.argv[2]
 except:
   logging.critical("Need to pass both project_id and train dataset path")
   sys.exit(1)
@@ -40,25 +38,40 @@ logging.info(f"TRAIN_PATH {train_path}")
 #
 # Read dataset
 #
-#fields = """doc_id,hotel_name,hotel_url,street,city,state,country,zip,class,price,
-#num_reviews,CLEANLINESS,ROOM,SERVICE,LOCATION,VALUE,COMFORT,overall_ratingsource""".replace("\n",'').split(",")
 
-read_table_opts = dict(sep="\t", names=fields, index_col=False)
+# real_numeric_features = ["if" + str(i) for i in range(1, 14)]
+# real_categorical_features = ["cf" + str(i) for i in range(1, 27)] + ["day_number"]
+# real_fields = ["id", "label"] + real_numeric_features + real_categorical_features
+
+
+read_table_opts = dict(sep="\t", 
+		                   names=real_fields,
+		                   usecols=fields, 
+        	             index_col=False)
 df = pd.read_table(train_path, **read_table_opts)
+
+#
+# cutting columns for train
+#
+
+cols_to_drop = ['id', 'label']
+
 
 #split train/test
 X_train, X_test, y_train, y_test = train_test_split(
-    df.iloc[:100000,2:], df.iloc[:100000,1], test_size=0.33, random_state=42
+    df.drop(columns=cols_to_drop, inplace=False),
+    df['label'],
+    test_size=0.33,
+    random_state=42
 )
 
 #
 # Train the model
 #
+
 model.fit(X_train, y_train)
 
-y_pred = model.predict_proba(X_test)
-
-model_score = log_loss(y_test, y_pred[:, 1])
+model_score = model.score(X_test, y_test)
 
 logging.info(f"model score: {model_score:.3f}")
 
