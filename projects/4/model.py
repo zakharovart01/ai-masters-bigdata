964d35b1fa355e2d4f54ad9d67bb9caa3b7d22fe
Artemii <zakhar@DESKTOP-ECRNMH1.localdomain>
2023-05-18 20:30:37 +0300

    4

--- projects/4/model.py
+++ projects/4/model.py
@@ -1,59 +1,42 @@
 #!/opt/conda/envs/dsenv/bin/python
 
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml.feature import HashingTF, Tokenizer, VectorAssembler
+from pyspark.ml import Pipeline
+from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.sql.types import *
 from pyspark.ml.feature import *
-from pyspark.ml.classification import LogisticRegression
-from pyspark.ml import Estimator, Transformer
-from pyspark.ml import Pipeline
-from pyspark.sql import functions as f
-from pyspark.ml.param.shared import HasInputCol, HasOutputCol, Param, Params, TypeConverters
-from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
-from pyspark.sql import DataFrame
-from pyspark import keyword_only
+import pyspark.sql.functions as f
 
-class FillNaTransformer(Transformer, HasInputCol, HasOutputCol, DefaultParamsReadable, DefaultParamsWritable):
-    input_col = Param(Params._dummy(), "input_col", "input column name.", typeConverter=TypeConverters.toString)
-    output_col = Param(Params._dummy(), "output_col", "output column name.", typeConverter=TypeConverters.toString)
-    val = Param(Params._dummy(), "val", "value for filling nan.", typeConverter=TypeConverters.toInt)
-  
-    @keyword_only
-    def __init__(self, input_col: str = "input", output_col: str = "output", val: int = 0):
-        super(FillNaTransformer, self).__init__()
-        self._setDefault(input_col=None, output_col=None, val = 0)
-        kwargs = self._input_kwargs
-        self.set_params(**kwargs)
-    
-    @keyword_only
-    def set_params(self, input_col: str = "input", output_col: str = "output", val: int = 0):
-        kwargs = self._input_kwargs
-        self._set(**kwargs)
-    
-    def get_input_col(self):
-        return self.getOrDefault(self.input_col)
-  
-    def get_output_col(self):
-        return self.getOrDefault(self.output_col)
-  
-    def get_val(self):
-        return self.getOrDefault(self.val)
-    
-    def _transform(self, df: DataFrame):
-        df = df.drop("reviewTime", "reviewerName", "unixReviewTime").cache()
-        df = df.withColumn("vote", df["vote"].cast(IntegerType()))
-        df = df.withColumn("comment_length", f.length(df.reviewText))
-        df = df.na.fill(0)
-        return df
-  
-custom_transformer = FillNaTransformer()
-tokenizer = Tokenizer(inputCol="reviewText", outputCol="text")
-hasher = HashingTF(numFeatures=100, binary=True, inputCol=tokenizer.getOutputCol(), outputCol="word_vector")
-assembler = VectorAssembler(inputCols=[hasher.getOutputCol(), "comment_length", "vote"], outputCol="features")
-lr = LogisticRegression(labelCol="overall", maxIter=25)
+
+tokenizer = Tokenizer(inputCol="reviewText",
+                      outputCol="reviewTextTokenized")
+
+stop_word_remover = StopWordsRemover(inputCol=tokenizer.getOutputCol(),
+                                     outputCol="reviewTextFiltered",
+                                     stopWords=StopWordsRemover.loadDefaultStopWords("english"))
+
+count_vectorizer = CountVectorizer(inputCol=stop_word_remover.getOutputCol(),
+                                   outputCol="reviewVector",
+                                   binary=True,
+                                   vocabSize=2500)
+
+vector_assembler = VectorAssembler(inputCols=[count_vectorizer.getOutputCol(), 'verified'],
+                                   outputCol="features")
+
+lr = LogisticRegression(featuresCol="reviewVector",
+                        labelCol="overall",
+                        maxIter=10,
+                        regParam=0)
+
+evaluator = RegressionEvaluator(labelCol="overall",
+                                predictionCol="prediction",
+                                metricName='rmse')
 
 pipeline = Pipeline(stages=[
-    custom_transformer,
-    tokenizer,
-    hasher,
-    assembler,
-    lr
+                    tokenizer,
+                    stop_word_remover,
+                    count_vectorizer,
+                    vector_assembler,
+                    lr
 ])
--- projects/4/predict.py
+++ projects/4/predict.py
@@ -5,18 +5,15 @@ import sys
 from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.getOrCreate()
-spark.sparkContext.setLogLevel('WARN')
 
 from pyspark.ml import Pipeline, PipelineModel
+from train import schema
 
-model_path = sys.argv[1] 
+model_path = sys.argv[1]
 test_data = sys.argv[2]
 output = sys.args[3]
 
 model = PipelineModel.load(model_path)
-
-from train import schema
-
 df = spark.read\
           .schema(schema)\
           .format("json")\
